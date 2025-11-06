5220104e8f22e851cb7a986ce5faf35baf96c915
Stephane Bisson <stephane.c.bisson@gmail.com>
2025-11-06 13:11:01 -0500

    SDG: name

--- src/config/sdg.js
+++ src/config/sdg.js
@@ -1,5 +1,5 @@
 export const SDG_INFO = {
-  name: 'L\'enfantaisie',
+  name: 'L\'Enfantaisie',
   addressLine1: '5435 av. Notre-Dame-de-Grâce',
   addressLine2: 'Montréal (Québec) H4A 1L2',
   phone: '5145965425',
